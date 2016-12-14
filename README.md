0f521231df84350ab219cc8b3cd45f5acb030648
Simon Parent <sboparen@gmail.com>
2016-12-14 15:29:37 -0500

    Starting point.

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+git clone --recursive https://github.com/sboparen/env && env/install.sh
+```
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+makelink() {
+    ln -s "$2" "$1" 2>/dev/null
+    test -h "$1" || tput setaf 1
+    file -h "$1"
+    tput sgr0
+}
+makelink .bashrc env/bashrc
