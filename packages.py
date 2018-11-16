7ad31dfa545b1151a1dc97a318c0edfff3c4535b
Simon Parent <sboparen@gmail.com>
2018-11-16 18:16:09 -0500

    Add more packages to install.

--- packages.py
+++ packages.py
@@ -3,6 +3,7 @@ packages = '''
 ack-grep:apt ack:pacman
 acpi
 adb:apt
+alsa-utils:pacman
 anki
 audacity
 build-essential:apt base-devel:pacman
@@ -15,22 +16,25 @@ encfs
 fdupes
 feh
 ffmpeg
+firefox:pacman
 gedit
 git
-ibus-anthy
 imagemagick
 iotop
 jekyll:apt
 jmtpfs:apt
 libx11-dev:apt libx11:pacman
 libxinerama-dev:apt libxinerama:pacman
+mesa-demos:pacman
 mpv
 nautilus-dropbox:apt
 nload
+openssh:pacman
 otf-ipafont:pacman
 p7zip-full:apt
 pdftk:apt
 pypy
+python2-xlib:pacman
 python-pip:apt
 python-pygame:apt
 rhino
@@ -38,17 +42,24 @@ rtorrent
 ruby
 sharutils:pacman
 sloccount
+slock:pacman
 stalonetray
 suckless-tools:apt dmenu:pacman
+sudo:pacman
 tidy
 tig
+time:pacman
 ttf-mscorefonts-installer:apt
 unclutter
 vim
 vorbis-tools:pacman
+wicd-gtk:pacman
 xclip
+xorg-fonts-misc:pacman
+xorg:pacman
 xpdf
 xterm
+zip:pacman unzip:pacman
 '''.split()
 
 def packages_for(platform):
